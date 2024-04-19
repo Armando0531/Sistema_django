e4aa3ca664f94b03a7e74e054c5f07a93b1d9c0f
Armando <armandoenriquez0531@gmail.com>
2024-04-19 02:30:12 -0600

    Se creo un usuario para el administrador de django , se establecio la visuaizacion de lista y se añadieron alumnos

--- abcc_django/Apps/Alumno/models.py
+++ abcc_django/Apps/Alumno/models.py
@@ -5,3 +5,6 @@ class Alumno(models.Model):
     numcontrol = models.CharField(max_length=255)
     nombre = models.CharField(max_length=255)
     semestre = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.numcontrol} {self.nombre} {self.semestre}"
